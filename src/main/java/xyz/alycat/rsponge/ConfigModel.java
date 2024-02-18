c78f51c7b5d872c172bac810eb857ef8d9941bd5
Allison Smith <allisondaniellesmith@protonmail.com>
2024-02-18 22:45:18 +1100

    Remove uneeded config sync

--- src/main/java/xyz/alycat/rsponge/ConfigModel.java
+++ src/main/java/xyz/alycat/rsponge/ConfigModel.java
@@ -1,14 +1,13 @@
 package xyz.alycat.rsponge;
 
-import io.wispforest.owo.config.Option;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 import io.wispforest.owo.config.annotation.RestartRequired;
-import io.wispforest.owo.config.annotation.Sync;
 @Modmenu(modId = Main.MOD_ID)
 @Config(name = Main.MOD_ID, wrapperName = "ModConfig")
 public class ConfigModel {
+    // Client side
+    // Server handles the recipe config, no need to sync
     @RestartRequired
-    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public float rarity = 0.16f;
 }
