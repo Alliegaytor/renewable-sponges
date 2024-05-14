861cd08b43b16004b36a58738da4094e64654cf6
Allison Smith <allisondaniellesmith@protonmail.com>
2024-05-14 15:57:28 +1000

    1.20.5+ refactor
    * Update for Minecraft 1.20.5+
    * Change how sponges are appended to fishing loot table
        * Sponges now are dropped alone without any other item
        * Sponges are now in the FISHING_GAMEPLAY loot table
        * Rarity is now calculated as weight
        * Rarity set to 0.12 (12 weight) by default
    * Used newer build tools

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Renewable sponges
 
-A simple Minecraft mod that adds sponges to the fishing treasure reward loot table.
+A simple Minecraft mod that adds sponges to the fishing loot table.
 
-The rarity value is 0.16 (16%) by default, but can be configured in the settings.
+The weight in the table is 12 (0.12 in the config) by default, but can be configured in the settings.
 
 It is technically not needed client-side if you join a server with this mod.
--- src/main/java/xyz/alycat/rsponge/ModifyLootTable.java
+++ src/main/java/xyz/alycat/rsponge/ModifyLootTable.java
@@ -2,19 +2,15 @@ package xyz.alycat.rsponge;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.item.Item;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.util.Identifier;
+import net.minecraft.registry.RegistryKey;
 
 public class ModifyLootTable {
-    public static void addItemToLootTable(Identifier tableId, Item item, Float rarity) {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if (source.isBuiltin() && tableId.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .conditionally(RandomChanceLootCondition.builder(rarity))
-                        .with(ItemEntry.builder(item));
-                tableBuilder.pool(poolBuilder);
+    public static void addItemToLootTable(RegistryKey<LootTable> tableId, Item item, Integer weight) {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+            if (source.isBuiltin() && tableId.equals(key)) {
+                tableBuilder.modifyPools(poolBuilder -> poolBuilder.with(ItemEntry.builder(item).weight(weight)));
             }
         });
     }
