60d033b85982ce1f8a9cb530edcf19e7dd99342e
Alliegaytor <contact@alycat.xyz>
2025-05-19 11:49:40 +1000

    Upgrade LootTableEvents fabric api from v2 -> v3
    - v2 subject to removal

--- src/main/java/xyz/alycat/rsponge/ModifyLootTable.java
+++ src/main/java/xyz/alycat/rsponge/ModifyLootTable.java
@@ -1,6 +1,6 @@
 package xyz.alycat.rsponge;
 
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.item.Item;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.entry.ItemEntry;
@@ -8,7 +8,7 @@ import net.minecraft.registry.RegistryKey;
 
 public class ModifyLootTable {
 	public static void addItemToLootTable(RegistryKey<LootTable> tableId, Item item, Integer weight) {
-		LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+		LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
 			if (source.isBuiltin() && tableId.equals(key)) {
 				tableBuilder.modifyPools(poolBuilder -> poolBuilder.with(ItemEntry.builder(item).weight(weight)));
 			}
